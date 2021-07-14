e0737a9f763a316f9ca79b352c53f4842617ce84
Sahrul-ux <sahrulrajab11@gmail.com>
2021-07-14 22:36:05 +0700

    menambahkan standar output atau untuk mencetak output

--- pertemuan2/index.php
+++ pertemuan2/index.php
@@ -9,7 +9,19 @@
 </head>
 
 <body>
-    <!-- ini adalah komen -->
+    <?php    //ini adalah komen
+
+    //Pertemuan 2 - PHP Dasar
+    //Sintaks PHP
+
+    //Standar Output
+    //echo, print untuk mencetak variable
+    //print_r, untuk mencetak isi array
+    //var_dump, untuk melihat isi variable ngebuging
+
+    echo "Sahrul";
+    ?>
+
 </body>
 
 </html>
