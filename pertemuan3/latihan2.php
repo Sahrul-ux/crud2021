1c9e0d1d57420d3050f6763b5024589321d41310
Sahrul-ux <sahrulrajab11@gmail.com>
2021-07-17 22:50:09 +0700

    contoh pengkondisian if, else if, else..

--- pertemuan3/latihan2.php
+++ pertemuan3/latihan2.php
@@ -5,8 +5,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Latian Pengkondisian</title>
 </head>
+<?php
+// if else 
+// if else if else 
+// ternary 
+// switch 
+
+$x = 20;
+if ($x < 20) {
+    echo "benar";
+} elseif ($x == 20) {
+    echo "bingo!";
+} else {
+    echo "Salah!";
+}
+
+
+
+?>
 
 <body>
 
