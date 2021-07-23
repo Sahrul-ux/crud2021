aafdaf21f876d6b627106997169de3e4ddb8c79f
Sahrul-ux <sahrulrajab11@gmail.com>
2021-07-23 23:06:42 +0700

    cara menulis array ada cara baru dan cara lama

--- pertemuan5/latihan1.php
+++ pertemuan5/latihan1.php
@@ -1,1 +1,10 @@
 <?php
+
+// array
+// variable yang memiliki banyak nilai
+
+// membuat array
+// cara lama
+$hari = array("Senin", "Selasa", "Rabu");
+// cara baru
+$bulan = ["Januari", "Februari", "Maret"];
