f711aaf459e097c7afd4bf6535313ad8f8231871
Sahrul-ux <sahrulrajab11@gmail.com>
2021-07-23 23:10:50 +0700

    contoh array dengan type data berbeda

--- pertemuan5/latihan1.php
+++ pertemuan5/latihan1.php
@@ -8,3 +8,6 @@
 $hari = array("Senin", "Selasa", "Rabu");
 // cara baru
 $bulan = ["Januari", "Februari", "Maret"];
+
+// isi array boleh dengan type data berbeda string, char, bool
+$arr1 = [123, "tulisan", false];
