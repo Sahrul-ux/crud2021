1c23e854e312bdbc82a6660088cf6f24198225b3
Sahrul-ux <sahrulrajab11@gmail.com>
2021-07-23 23:19:58 +0700

    cara menampilkan array

--- pertemuan5/latihan1.php
+++ pertemuan5/latihan1.php
@@ -2,6 +2,9 @@
 
 // array
 // variable yang memiliki banyak nilai
+// elemen pada array boleh memiliki tipe data yang berbeda
+// pasangan antara key dan value
+// key-nya adalah index, yang mulai dari 0
 
 // membuat array
 // cara lama
@@ -11,3 +14,6 @@ $bulan = ["Januari", "Februari", "Maret"];
 
 // isi array boleh dengan type data berbeda string, char, bool
 $arr1 = [123, "tulisan", false];
+
+// menampilkan array
+var_dump($hari);
