1312b0efd69b2356c87e770d25eb8f565566972a
Sahrul-ux <sahrulrajab11@gmail.com>
2021-07-24 22:15:27 +0700

    mencetak array dua dimensi

--- pertemuan5/latihan3.php
+++ pertemuan5/latihan3.php
@@ -1,5 +1,8 @@
 <?php
-$kucing = ["Oren", 3, "Heru", "Wishkas", "cempaka putih"];
+$kucing = [
+    ["Oren", 3, "Heru", "Wishkas", "cempaka putih"],
+    ["Loreng", 12, "Aldi", "Wishkas", "Kayu Putih"]
+];
 
 ?>
 <!DOCTYPE html>
@@ -14,13 +17,15 @@ $kucing = ["Oren", 3, "Heru", "Wishkas", "cempaka putih"];
 
 <body>
     <h1>Daftar Kucing</h1>
-    <ul>
-        <li><?php echo $kucing[0]; ?></li>
-        <li><?php echo $kucing[1]; ?></li>
-        <li><?php echo $kucing[2]; ?></li>
-        <li><?php echo $kucing[3]; ?></li>
-        <li><?php echo $kucing[4]; ?></li>
-    </ul>
+    <?php foreach ($kucing as $k) : ?>
+        <ul>
+            <li><?php echo $k[0]; ?></li>
+            <li><?php echo $k[1]; ?></li>
+            <li><?php echo $k[2]; ?></li>
+            <li><?php echo $k[3]; ?></li>
+            <li><?php echo $k[4]; ?></li>
+        </ul>
+    <?php endforeach; ?>
 </body>
 
 </html>
