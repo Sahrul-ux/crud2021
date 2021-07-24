537c2b58669725af6fd1fb53997d12266550f9a8
Sahrul-ux <sahrulrajab11@gmail.com>
2021-07-24 16:30:07 +0700

    menambahkan css

--- pertemuan5/latihan2.php
+++ pertemuan5/latihan2.php
@@ -1,3 +1,8 @@
+<?php
+// pengulangan pada array 
+// for / foreach
+$angka = [3, 2, 15, 20, 11, 77, 89];
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,9 +11,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Latihan Function</title>
+    <style>
+        div {
+            width: 50px;
+            height: 50px;
+            background-color: salmon;
+            text-align: center;
+            line-height: 50px;
+            margin: 3px;
+            float: left;
+        }
+    </style>
+
 </head>
 
 <body>
+    <?php for() { ?>
+    <div>3</div>
+    <?php } ?>
 
 </body>
 
