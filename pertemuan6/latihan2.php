113a079c75e8f714396305e14e826f3750dae438
Sahrul-ux <sahrulrajab11@gmail.com>
2021-07-25 12:13:41 +0700

    memberikan css hingga berbentuk otak jajar ke kanan

--- pertemuan6/latihan2.php
+++ pertemuan6/latihan2.php
@@ -6,6 +6,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Latihan Array</title>
+    <style>
+        .kotak {
+            width: 30px;
+            height: 30px;
+            background-color: aquamarine;
+            text-align: center;
+            line-height: 30px;
+            margin: 3px;
+            float: left;
+        }
+    </style>
 </head>
 
 <body>
