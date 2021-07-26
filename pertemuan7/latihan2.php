9e8411aa666767d3ae8178a0dda57f5f4aa6dbcd
Sahrul-ux <sahrulrajab11@gmail.com>
2021-07-26 19:35:00 +0700

    mencoba mengirimkan data menggunanakan metode request variabel get

--- pertemuan7/latihan2.php
+++ pertemuan7/latihan2.php
@@ -10,6 +10,7 @@
 // $_ENV
 
 // 1. $_GET
-$_GET["jeniskucing"] = "persia";
-$_GET["warna"] = "oren";
+// $_GET["jeniskucing"] = "persia";
+// $_GET["warna"] = "oren";
+// var_dump($_GET);
 var_dump($_GET);
