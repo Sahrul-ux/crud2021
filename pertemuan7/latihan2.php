032fcc490bb7e641887714b75382e801eb6a05ca
Sahrul-ux <sahrulrajab11@gmail.com>
2021-07-26 20:35:06 +0700

    membuat array dan mencetaknya

--- pertemuan7/latihan2.php
+++ pertemuan7/latihan2.php
@@ -13,4 +13,64 @@
 // $_GET["jeniskucing"] = "persia";
 // $_GET["warna"] = "oren";
 // var_dump($_GET);
-var_dump($_GET);
+$kucing = [
+    [
+        "jeniskucing" => "Anggora",
+        "warna" => "orange",
+        "namakucing" => "Bobon",
+        "makanan" => "Wishkas",
+        "usia" => 3,
+        "namapemilik" => "Heru",
+        "notelp" => "081",
+        "alamat" => "cempaka putih",
+        "email" => "heru@gmail.com",
+        "gambar" => "binaniaga2a.jpg"
+    ],
+    [
+        "jeniskucing" => "Persia",
+        "warna" => "abu-abu",
+        "namakucing" => "Woro",
+        "makanan" => "Wishkas",
+        "usia" => 12,
+        "namapemilik" => "Satria",
+        "notelp" => "089",
+        "alamat" => "Celilitaan",
+        "email" => "woro@gmail.com",
+        "gambar" => "binaniaga3.JPG"
+    ],
+    [
+        "jeniskucing" => "Mixdom",
+        "warna" => "Putih",
+        "namakucing" => "Angel",
+        "makanan" => "Royal canin",
+        "usia" => 6,
+        "namapemilik" => "Seto",
+        "notelp" => "088",
+        "alamat" => "Pulo Gadung",
+        "email" => "seto@gmail.com",
+        "gambar" => "binaniaga6.jpg"
+    ]
+];
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GET</title>
+</head>
+
+<body>
+    <h1>Daftar Pemilik Penitipan Kucing</h1>
+    <ul>
+        <?php foreach ($kucing as $k) : ?>
+            <li>
+                <a href="latihan3.php"> <?= $k["namapemilik"]; ?></a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</body>
+
+</html>
