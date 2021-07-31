470d0b372677b4f3e7431c7ef11f9d8bc15a4cf4
Sahrul-ux <sahrulrajab11@gmail.com>
2021-07-31 17:17:05 +0700

    memunculkan data di db tapi masih manual

--- pertemuan9/index.php
+++ pertemuan9/index.php
@@ -15,9 +15,9 @@ $result = mysqli_query($conn, "SELECT * FROM tb_kucing");
 // mysqli_fetch_array() -> mengembalikan keduanya (data jadi doble)
 // mysqli_fetch_object() -> mengembalikan objek
 
-while ($k = mysqli_fetch_assoc($result))
-    var_dump($k["namakucing"]);
-
+// while ($k = mysqli_fetch_assoc($result)) {
+//     var_dump($k);
+// }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -40,32 +40,34 @@ while ($k = mysqli_fetch_assoc($result))
             <th>Warna</th>
             <th>Nama Kucing</th>
             <th>Makanan</th>
-            <th>Usia</th>
+            <th>Usia (bln.)</th>
             <th>Nama Pemilik</th>
             <th>Alamat</th>
             <th>No. Telp</th>
             <th>Email</th>
             <th>Foto Pemilik</th>
         </tr>
-        <tr>
-            <td>1</td>
-            <td>
-                <a href="">ubah</a> |
-                <a href="">hapus</a>
-            </td>
-            <td><img src="kucing/kucing1.jpg" width="100"></td>
-            <td>Jenis Kucing</td>
-            <td>Warna</td>
-            <td>Nama Kucing</td>
-            <td>Makanan</td>
-            <td>Usia</td>
-            <td>Nama Pemilik</td>
-            <td>Alamat</td>
-            <td>No. Telp</td>
-            <td>Email</td>
-            <td><img src="img/gambar1.jpg" width="100"></td>
-        </tr>
-    </table>
+
+        <?php while ($row = mysqli_fetch_assoc($result)) : ?>
+            <tr>
+                <td>1</td>
+                <td>
+                    <a href="">ubah</a> |
+                    <a href="">hapus</a>
+                </td>
+                <td><img src="kucing/<?php echo $row["fotokucing"]; ?>" width="100"></td>
+                <td><?= $row["jeniskucing"]; ?></td>
+                <td><?= $row["warna"]; ?></td>
+                <td><?= $row["namakucing"]; ?></td>
+                <td><?= $row["makanan"]; ?></td>
+                <td><?= $row["usia"]; ?></td>
+                <td><?= $row["namapemilik"]; ?></td>
+                <td><?= $row["alamat"]; ?></td>
+                <td><?= $row["notelp"]; ?></td>
+                <td><?= $row["jeniskucing"]; ?></td>
+                <td><img src="img/<?= $row["fotopemilik"]; ?>" width="100"></td>
+            </tr>
+        <?php endwhile; ?>
 </body>
 
 </html>
