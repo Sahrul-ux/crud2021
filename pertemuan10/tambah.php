505b3e226a3f804da7c1435792a8b8813ec9ee44
Sahrul-ux <sahrulrajab11@gmail.com>
2021-08-01 22:56:58 +0700

    belajar insert query

--- pertemuan10/tambah.php
+++ pertemuan10/tambah.php
@@ -1,6 +1,27 @@
 <?php
+// koneksi ke DBMS
+$conn = mysqli_connect("localhost", "root", "", "kucing");
+// cek apakah tombol submit sudah ditekan atau belum
 if (isset($_POST["submit"])) {
-    var_dump($_POST);
+    // var_dump($_POST);
+    // ambil apakah tombol submit sudah ditekan atau belum
+    $jeniskucing = $_POST["jeniskucing"];
+    $warna = $_POST["warna"];
+    $namakucing = $_POST["namakucing"];
+    $makanan = $_POST["makanan"];
+    $usia = $_POST["usia"];
+    $fotokucing = $_POST["fotokucing"];
+    $namapemilik = $_POST["namapemilik"];
+    $alamat = $_POST["alamat"];
+    $notelp = $_POST["notelp"];
+    $email = $_POST["email"];
+    $fotopemilik = $_POST["fotopemilik"];
+
+    // query insert data
+    $query = "INSERT INTO tb_kucing
+    VALUES
+    ('', '$jeniskucing','$warna', '$namakucing','$makanan', '$usia', '$fotokucing', '$namapemilik', '$alamat', '$notelp','$email', '$fotopemilik')";
+    mysqli_query($conn, $query);
 }
 
 ?>
