f6edfb47fad59369dc9104f81e3415c7315291f0
Sahrul-ux <sahrulrajab11@gmail.com>
2021-08-08 21:22:52 +0700

    membuat function tambah variabel data atau variabel post

--- pertemuan10/functions.php
+++ pertemuan10/functions.php
@@ -12,3 +12,19 @@ function query($query)
     }
     return $rows;
 }
+
+function tambah($sata)
+{
+    // ambil apakah tombol submit sudah ditekan atau belum
+    $jeniskucing = $_POST["jeniskucing"];
+    $warna = $_POST["warna"];
+    $namakucing = $_POST["namakucing"];
+    $makanan = $_POST["makanan"];
+    $usia = $_POST["usia"];
+    $fotokucing = $_POST["fotokucing"];
+    $namapemilik = $_POST["namapemilik"];
+    $alamat = $_POST["alamat"];
+    $notelp = $_POST["notelp"];
+    $email = $_POST["email"];
+    $fotopemilik = $_POST["fotopemilik"];
+}
--- pertemuan10/tambah.php
+++ pertemuan10/tambah.php
@@ -4,18 +4,6 @@ $conn = mysqli_connect("localhost", "root", "", "kucing");
 // cek apakah tombol submit sudah ditekan atau belum
 if (isset($_POST["submit"])) {
     // var_dump($_POST);
-    // ambil apakah tombol submit sudah ditekan atau belum
-    $jeniskucing = $_POST["jeniskucing"];
-    $warna = $_POST["warna"];
-    $namakucing = $_POST["namakucing"];
-    $makanan = $_POST["makanan"];
-    $usia = $_POST["usia"];
-    $fotokucing = $_POST["fotokucing"];
-    $namapemilik = $_POST["namapemilik"];
-    $alamat = $_POST["alamat"];
-    $notelp = $_POST["notelp"];
-    $email = $_POST["email"];
-    $fotopemilik = $_POST["fotopemilik"];
 
     // query insert data
     $query = "INSERT INTO tb_kucing
